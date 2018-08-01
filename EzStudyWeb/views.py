aeb8533044ae26250babdb9c21bafb08f99ae455
Tey Yi Chuan <bluepegasusxx@gmail.com>
2018-08-01 13:54:31 +0800

    try to use dialogflow directly

--- EzStudyWeb/views.py
+++ EzStudyWeb/views.py
@@ -14,7 +14,7 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 
-auth_firebase = firebase.auth()
+aute = firebase.auth()
 database = firebase.database()
 
 
@@ -32,7 +32,7 @@ def homepage(request):
     email = request.POST.get('email')
     password = request.POST.get('password')
     try:
-        user = auth_firebase.sign_in_with_email_and_password(email, password)
+        user = aute.sign_in_with_email_and_password(email, password)
     except:
         messages = "invalid users"
         return render(request, "LogIn.html", {"messg": messages})
@@ -51,7 +51,7 @@ def DoneSignUp(request):
     email = request.POST.get('email')
     password = request.POST.get('password')
     try:
-        user = auth_firebase.create_user_with_email_and_password(email, password)
+        user = aute.create_user_with_email_and_password(email, password)
         uid = user['localId']
         data = {"name": name, "status": "1"}
         database.child("users").child(uid).child("details").set(data)
@@ -98,55 +98,5 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
         print('Fulfillment text: {}\n'.format(
             response.query_result.fulfillment_text))
 
-def detect_intent_audio(project_id, session_id, audio_file_path,language_code):
-    """Returns the result of detect intent with an audio file as input.
-
-    Using the same `session_id` between requests allows continuation
-    of the conversaion."""
-    import dialogflow_v2 as dialogflow
-
-    session_client = dialogflow.SessionsClient()
-
-    # Note: hard coding audio_encoding and sample_rate_hertz for simplicity.
-    audio_encoding = dialogflow.enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16
-    sample_rate_hertz = 16000
-
-    session = session_client.session_path(project_id, session_id)
-    print('Session path: {}\n'.format(session))
-
-    with open(audio_file_path, 'rb') as audio_file:
-        input_audio = audio_file.read()
-
-    audio_config = dialogflow.types.InputAudioConfig(
-        audio_encoding=audio_encoding, language_code=language_code,
-        sample_rate_hertz=sample_rate_hertz)
-    query_input = dialogflow.types.QueryInput(audio_config=audio_config)
-
-    response = session_client.detect_intent(
-        session=session, query_input=query_input,
-        input_audio=input_audio)
-
-    print('=' * 20)
-    print('Query text: {}'.format(response.query_result.query_text))
-    print('Detected intent: {} (confidence: {})\n'.format(
-        response.query_result.intent.display_name,
-        response.query_result.intent_detection_confidence))
-    print('Fulfillment text: {}\n'.format(
-        response.query_result.fulfillment_text))
-
-def detect_intent_stream(project_id, session_id, audio_file_path,language_code):
-    """Returns the result of detect intent with streaming audio as input.
-
-    Using the same `session_id` between requests allows continuation
-    of the conversaion."""
-    import dialogflow_v2 as dialogflow
-    session_client = dialogflow.SessionsClient()
-
-    # Note: hard coding audio_encoding and sample_rate_hertz for simplicity.
-    audio_encoding = dialogflow.enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16
-    sample_rate_hertz = 16000
-
-    session_path = session_client.session_path(project_id, session_id)
-    print('Session path: {}\n'.format(session_path))
 
 
